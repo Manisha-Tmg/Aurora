52813ee696403f82ffaa2e483999d27751ffb2b7
Manisha Tamang <manishatamang752@gmail.com>
2026-02-17 10:00:00 +0545

    Feat:models implement

---
+++ backend/src/config/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+export class App {
+  public app = express();
+
+  constructor() {
+    this.initializeMiddleware();
+    this.initializeRoutes();
+  }
+
+  private initializeMiddleware() {
+    this.app.use(
+      cors({
+        origin: "http://localhost/5135",
+        credentials: true,
+      }),
+    );
+    this.app.use(express.json());
+  }
+  private initializeRoutes() {
+    // define routes
+  }
+}
