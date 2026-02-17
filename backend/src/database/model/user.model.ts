cfdd3ab6bde10341a2997936a1c3c50ef3498fb2
Manisha Tamang <manishatamang752@gmail.com>
2026-02-17 10:00:00 +0545

    Feat:models implement

---
+++ backend/src/database/model/user.model.ts
@@ -0,0 +1,56 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { UserAttributes, UserCreationAttributes } from "../../types/user.types";
+
+@Table({
+  tableName: "users",
+  modelName: "User",
+  timestamps: true,
+})
+export default class User extends Model<
+  UserAttributes,
+  UserCreationAttributes
+> {
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+    primaryKey: true,
+  })
+  declare id: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare name: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare email: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare password: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare address: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare phone: string;
+
+  @Column({
+    type: DataType.ENUM("user", "admin", "superAdmin"),
+    allowNull: false,
+    defaultValue: "user",
+  })
+  declare role: "user" | "admin" | "superAdmin";
+}
