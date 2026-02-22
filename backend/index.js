5cd6f55f25ba409835b7da24a6499d841eba2543
Manisha Tamang <manishatamang752@gmail.com>
2026-02-22 10:00:00 +0545

    Routes Created

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import express, { json } from "express";
 import connectToDb from "./src/connectToDb/connectToDb.js";
 import cors from "cors";
 import userRouter from "./src/Routes/userRouter.js";
+import fileRouter from "./src/controllers/fileRouter.js";
+import pageNotFound from "./src/middleware/pageNotFound.js";
+import errorMiddleware from "./src/middleware/errorMiddleware.js";
 
 const app = express();
 app.use(cors());
@@ -16,3 +19,10 @@ app.use(express.static("public"));
 app.use(json());
 
 app.use("/user", userRouter);
+app.use("/file", fileRouter);
+
+// url middleware
+app.use("/", pageNotFound);
+
+// error middleware
+app.use(errorMiddleware);
