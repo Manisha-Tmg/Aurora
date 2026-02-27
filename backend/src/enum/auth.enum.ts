2d1e7e824a648ad95ad5566ce08837637c37a156
Manisha Tamang <manishatamang752@gmail.com>
2026-02-27 10:00:00 +0545

    Fix:More feat added

---
+++ backend/src/enum/auth.enum.ts
@@ -0,0 +1,15 @@
+export enum Role {
+  User = "user",
+  Admin = "admin",
+  Superadmin = "superadmin",
+}
+
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other",
+}
+export enum Status {
+  Active = "active",
+  InActive = "inActive",
+}
